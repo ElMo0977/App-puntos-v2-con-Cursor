4bb7d54d915167414ba6ae21e3c5f4f431b5ee73
Pablo Ramirez <molocura@gmail.com>
2025-09-28 15:04:50 +0200

    page 5

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/App-puntos-v2-con-Cursor/', // EXACTO al nombre del repo
   plugins: [react()],
+  base: '/App-puntos-v2-con-Cursor/', // respeta mayúsculas/minúsculas
 })
