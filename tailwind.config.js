5a35c0732371f1371c7685e7238f49c6c5372767
Pablo Ramirez <molocura@gmail.com>
2025-09-28 15:12:31 +0200

    page 6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+// tailwind.config.js
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: { extend: {} },
-  plugins: []
-};
+  plugins: [],
+}
